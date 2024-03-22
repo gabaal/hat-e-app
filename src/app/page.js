7a17d7e25b637282829ecdabecc512a0c535e014
gabaal <geraldbaalham@live.co.uk>
2024-03-22 14:20:51 +0000

    Gerald DB connected

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,9 @@
 import Image from "next/image";
+import GetProducts from "./components/products-sql";
+const products = await GetProducts()
 
-export default function Home() {
+
+export default async function Home() {
   return (
     <main>
       <div>

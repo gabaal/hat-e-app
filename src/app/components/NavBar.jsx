80bef3050f3e4dc59ef2a7ecd3e84a7bdb45032d
Graeme Gillies <graeme.a.gillies@gmail.com>
2024-03-22 11:50:53 +0000

    fixed NavBar bug and added styling to it

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -1,18 +1,52 @@
 import Link from "next/link";
+import Image from "next/image";
 export default function Navbar() {
-  <>
-    <nav className="bg-green-500 py-4">
-      <div className="container mx-auto flex justify-between items-center px-4">
-        <Link href="/">Home</Link>
+  return (
+    <>
+      <nav className="bg-green-500 py-4">
+        <div className="container mx-auto flex justify-between items-center px-4">
+          <div>
+            <Link href="/">
+              <Image
+                className="rounded-lg"
+                src="/Logo.png"
+                height={100}
+                width={100}
+                alt="Logo for Hat brand"
+              />
+            </Link>
+          </div>
+          <div className="hidden md:flex items-center space-x-6 ml-auto transition-all duration-500 ease-in-out">
+            <Link
+              href="/about-us"
+              className="hover:text-gray-900 hover:shadow-md rounded-md px-4 py-2 bg-green-300 text-black hover:bg-gray-300"
+            >
+              About us
+            </Link>
 
-        <Link href="/about-us">About us</Link>
+            <Link
+              href="/shop"
+              className="hover:text-gray-900 hover:shadow-md rounded-md px-4 py-2 bg-green-300 text-black hover:bg-gray-300"
+            >
+              All hats
+            </Link>
 
-        <Link href="/shop">All hats</Link>
+            <Link
+              href="/shop/caps"
+              className="hover:text-gray-900 hover:shadow-md rounded-md px-4 py-2 bg-green-300 text-black hover:bg-gray-300"
+            >
+              Caps
+            </Link>
 
-        <Link href="/shop/caps">Caps</Link>
-
-        <Link href="/shop/other-hats">beanies</Link>
-      </div>
-    </nav>
-  </>;
+            <Link
+              href="/shop/other-hats"
+              className="hover:text-gray-900 hover:shadow-md rounded-md px-4 py-2 bg-green-300 text-black hover:bg-gray-300"
+            >
+              Beanies
+            </Link>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 }

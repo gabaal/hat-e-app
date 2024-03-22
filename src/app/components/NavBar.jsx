3f08876ef7a7ba7f49c82a879fa85d15c4c8987b
Graeme Gillies <graeme.a.gillies@gmail.com>
2024-03-22 13:13:10 +0000

    changed the navbar to a lighter green and matched the buttons with bg of logo

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -3,15 +3,15 @@ import Image from "next/image";
 export default function Navbar() {
   return (
     <>
-      <nav className="bg-green-500 py-4">
+      <nav className="bg-green-300 py-2">
         <div className="container mx-auto flex justify-between items-center px-4">
           <div>
             <Link href="/">
               <Image
                 className="rounded-lg"
                 src="/Logo.png"
-                height={100}
-                width={100}
+                height={70}
+                width={70}
                 alt="Logo for Hat brand"
               />
             </Link>
@@ -19,28 +19,28 @@ export default function Navbar() {
           <div className="hidden md:flex items-center space-x-6 ml-auto transition-all duration-500 ease-in-out">
             <Link
               href="/about-us"
-              className="hover:text-gray-900 hover:shadow-md rounded-md px-4 py-2 bg-green-300 text-black hover:bg-gray-300"
+              className="hover:text-gray-900 hover:shadow-md rounded-md px-4 py-2 bg-[#AAE292] text-black hover:bg-gray-300"
             >
               About us
             </Link>
 
             <Link
               href="/shop"
-              className="hover:text-gray-900 hover:shadow-md rounded-md px-4 py-2 bg-green-300 text-black hover:bg-gray-300"
+              className="hover:text-gray-900 hover:shadow-md rounded-md px-4 py-2 bg-[#AAE292] text-black hover:bg-gray-300"
             >
               All hats
             </Link>
 
             <Link
               href="/shop/caps"
-              className="hover:text-gray-900 hover:shadow-md rounded-md px-4 py-2 bg-green-300 text-black hover:bg-gray-300"
+              className="hover:text-gray-900 hover:shadow-md rounded-md px-4 py-2 bg-[#AAE292] text-black hover:bg-gray-300"
             >
               Caps
             </Link>
 
             <Link
               href="/shop/other-hats"
-              className="hover:text-gray-900 hover:shadow-md rounded-md px-4 py-2 bg-green-300 text-black hover:bg-gray-300"
+              className="hover:text-gray-900 hover:shadow-md rounded-md px-4 py-2 bg-[#AAE292] text-black hover:bg-gray-300"
             >
               Beanies
             </Link>
